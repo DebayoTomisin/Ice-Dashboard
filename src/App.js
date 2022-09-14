4958e90812909e8b2dc6718445d3e2e47cff5b83
Debayo Tomisin <debayotomisin07@gmail.com>
2022-09-14 23:47:11 +0100

    chore/add color state & view mode to provider

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FiSettings } from "react-icons/fi";
-import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
-import {
-  Ecommerce,
-  Orders,
-  Calendar,
-  Employees,
-  Stacked,
-  Pyramid,
-  Customers,
-  Kanban,
-  Line,
-  Area,
-  Bar,
-  Pie,
-  Financial,
-  ColorPicker,
-  ColorMapping,
-  Editor,
-} from "./pages";
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import { useStateContext } from "./context/ContextProvider";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import AppLayout from "./components/Layout";
 import AppRouter from "./routes";
