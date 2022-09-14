7db91f88869b2215e99b2bdd8f56b2fc36795d39
Debayo Tomisin <debayotomisin07@gmail.com>
2022-09-14 21:30:10 +0100

    chore/retool app layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useStateContext } from "../context/ContextProvider";
+import { Navbar, Footer, Sidebar, ThemeSettings } from ".";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+const AppLayout = ({ children }) => {
+  const { activeMenu } = useStateContext();
+
+  return (
+    <div className="flex relative dark:bg-main-dark-bg">
+      <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+        <TooltipComponent content="Settings" position="Top">
+          <button
+            type="button"
+            className="text-3xl p-3 hover: drop-shadow-xl hover:bg-light-gray text-white"
+            style={{ background: "blue", borderRadius: "50%" }}
+          >
+            <FiSettings />
+          </button>
+        </TooltipComponent>
+      </div>
+
+      {activeMenu && (
+        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+          <Sidebar />
+        </div>
+      )}
+
+      <div
+        className={
+          activeMenu
+            ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+            : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+        }
+      >
+        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
+          <Navbar />
+        </div>
+
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
